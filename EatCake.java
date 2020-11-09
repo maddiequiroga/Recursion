4b9926c24eb43fb254ec78f8284c0bd4b2a49dbe
Madison <madison.c.quiroga-1@ou.edu>
2020-11-09 16:29:01 -0600

    changed output

--- EatCake.java
+++ EatCake.java
@@ -5,13 +5,17 @@ public class EatCake {
 		
 	}
 	
-	public void eatCake(Cake cake) {
-		
+	public static void eatCake(Cake cake) {
+		if(! cake.isEmpty()) {
+		cake.takeABite();
+		eatCake(cake);
+		}
 	}
 	
 	public static void main(String[] args) {
 		Cake cake = new Cake();
 		eatCake(cake);
-		System.out.println("Done eating cake.");
+		System.out.println("Done eating cake" +
+				"-ready to go back to Java 2!!");
 	}
 }
